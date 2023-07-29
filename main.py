7380f26a4696a7e67a436fd60f8ca6b0745b91fd
Agu <gkn060702@gmail.com>
2023-07-29 19:47:22 +0800

    update

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ async def create_or_get_channel(guild, channel_name):
 @client.event
 async def on_ready():
     global GUILD_INFO
+    global AUDIO_SOURCE
     GUILD_INFO = {}
 
     for guild in client.guilds:
@@ -27,6 +28,8 @@ async def on_ready():
             'channel': await create_or_get_channel(guild, CHANNEL_NAME)
         }
         print(f"Channel '{GUILD_INFO[guild.id]['channel'].name}' with ID '{GUILD_INFO[guild.id]['channel'].id}' created/obtained for Guild '{guild.name}' with ID '{guild.id}'.")
+    audio_file = "LauDaiTinhAi.opus"
+    AUDIO_SOURCE = discord.FFmpegPCMAudio(audio_file)
     print(f"Bot is ready. Logged in as {client.user}.")
 
 async def disconnect_if_empty(voice_channel):
@@ -35,15 +38,13 @@ async def disconnect_if_empty(voice_channel):
         await voice_channel.guild.voice_client.disconnect()
         print(f'Disconnected from voice channel: {voice_channel.name}')
 
-async def play_audio(voice_channel):
-    audio_file = "LauDaiTinhAi.opus"
+async def play_audio(voice_channel, audio_source):
     voice_client = voice_channel.guild.voice_client
 
     if voice_client.is_playing():
         voice_client.stop()
 
-    audio_source = discord.FFmpegPCMAudio(audio_file)
-    voice_client.play(audio_source, after=lambda e: print(f"Finished playing: {audio_file}"))
+    voice_client.play(audio_source)
 
 @client.event
 async def on_voice_state_update(member, before, after):
@@ -57,7 +58,7 @@ async def join_voice_channel(voice_channel):
     if voice_channel.guild.voice_client is None:
         await voice_channel.connect()
         print(f'Joined voice channel: {voice_channel.name}')
-        await play_audio(voice_channel)
+        await play_audio(voice_channel, AUDIO_SOURCE)
 
 client.run(TOKEN)
 
