1a8aa1c1af92be60e835732fdd9d7295e0a90cb9
Agu <gkn060702@gmail.com>
2023-07-31 02:01:01 +0800

    complete audio play

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import discord
-import os
 from discord.ext import commands
 import asyncio
+from discord import FFmpegPCMAudio
+from secret import TOKEN, CHANNEL_NAME
 
-TOKEN = os.getenv("TOKEN")
-CHANNEL_NAME = os.getenv("CHANNEL_NAME")
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
+CONNECT = False
 
 async def create_or_get_channel(guild, channel_name):
     existing_channel = discord.utils.get(guild.voice_channels, name=channel_name)
@@ -32,48 +31,62 @@ async def on_ready():
         }
         print(f"Channel '{GUILD_INFO[guild.id]['channel'].name}' with ID '{GUILD_INFO[guild.id]['channel'].id}' created/obtained for Guild '{guild.name}' with ID '{guild.id}'.")
     audio_file = "LauDaiTinhAi.opus"
-    AUDIO_SOURCE = discord.FFmpegPCMAudio(audio_file)
     print(f"Bot is ready. Logged in as {client.user}.")
 
 async def disconnect_if_empty(voice_channel):
+    global CONNECT
+    print('Disconnecting if empty')
     await asyncio.sleep(15)  # Wait for 15 seconds
     if len(voice_channel.members) == 1:  # Only the bot is present in the channel
         voice_client = voice_channel.guild.voice_client
 
         if voice_client.is_playing():
             voice_client.stop()
-        
+            print('Stopped playing audio')
+        CONNECT = False
         await voice_client.disconnect()
         print(f'Disconnected from voice channel: {voice_channel.name}')
-        CONNECT = False
 
-async def play_audio(voice_channel, audio_source):
+async def play_audio(voice_channel, audio_file):
+    print('Playing audio')
     voice_client = voice_channel.guild.voice_client
 
-    if voice_client.is_playing():
-        voice_client.stop()
+    while True:
+        if not voice_client or not voice_client.is_connected():
+            return
 
-    voice_client.play(audio_source)
+        audio_source = discord.FFmpegPCMAudio(audio_file)
+        voice_client.play(audio_source)
+
+        while voice_client.is_playing():
+            await asyncio.sleep(1)
+
+        # Add a 3-second delay before playing the audio again
+        await asyncio.sleep(3)
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if before.channel is not None and before.channel.name == CHANNEL_NAME and CONNECT == True:
-        # User left the bot's voice channel, check if the channel is empty now
-        await disconnect_if_empty(before.channel)
-    if before.channel is None and after.channel is not None:  # User joined a voice channel
+    print('Voice state updated')
+    global CONNECT
+    print(CONNECT)
+    if before.channel is not None and after.channel is None:  # User left a voice channel
+        print('User left a voice channel')
+        if CONNECT:
+            await disconnect_if_empty(before.channel)
+    elif before.channel is None and after.channel is not None:  # User joined a voice channel
+        print('User joined a voice channel')
         if after.channel.name == CHANNEL_NAME:
             voice_channel = after.channel
-            if CONNECT == False:
+            if not CONNECT:
                 await join_voice_channel(voice_channel)
 
 async def join_voice_channel(voice_channel):
-    global connected_to_voice_channel
+    print('Joining voice channel')
+    global CONNECT
+    CONNECT = True
     if voice_channel.guild.voice_client is None:
         await voice_channel.connect()
         print(f'Joined voice channel: {voice_channel.name}')
-        await play_audio(voice_channel, AUDIO_SOURCE)
-        CONNECT = True
+        await play_audio(voice_channel, "LauDaiTinhAi.opus")
 
 client.run(TOKEN)
-
-
